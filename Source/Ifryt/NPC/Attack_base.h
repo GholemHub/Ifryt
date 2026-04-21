9f4d63b8aa6060e83b2f4e92402cee401afc7ae7
Yerzy <masaporta@gmail.com>
2026-04-21 19:21:48 +0200

    Testing go

--- Source/Ifryt/NPC/Attack_base.h
+++ Source/Ifryt/NPC/Attack_base.h
@@ -11,33 +11,34 @@ UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class IFRYT_API UAttack_base : public UActorComponent
 {
 	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UAttack_base();
-
-	UPROPERTY(VisibleAnywhere, Category = Fire)
-	USceneComponent* FirePoint;
-
-	UPROPERTY(EditAnywhere, Category = Fire)
-	TSubclassOf<AActor> ProjectileClass;
-
-	UPROPERTY(EditAnywhere, Category = Fire)
-	float ProjectileSpeed = 1000.0f;
-
-	UPROPERTY(EditAnywhere, Category = Fire)
-	bool bUseFirePointRotation = true;
-
-	UFUNCTION(BlueprintCallable)
-	void Fire();
+public:
+    UAttack_base();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    virtual void BeginPlay() override;
 
-		
+public:
+    /** The class to spawn as a projectile */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+    TSubclassOf<AActor> ProjectileClass;
+
+    /** Speed applied to projectile via impulse */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+    float ProjectileSpeed = 2000.f;
+
+    /** Offset from owner root where projectile spawns */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+    FVector FirePointOffset = FVector(100.f, 0.f, 50.f);
+
+    /** If true, aims toward player. If false, uses owner forward. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+    bool bAimAtPlayer = true;
+
+    UFUNCTION(BlueprintCallable, Category = "Attack")
+    void Fire();
+
+private:
+    /** Created once in BeginPlay, attached to owner root */
+    UPROPERTY()
+    USceneComponent* FirePoint = nullptr;
 };
